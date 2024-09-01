ed23c452a33736e9b8bea1cccccaa1c5feab9090
PixelOmen <PixelOmen@gmail.com>
2024-09-01 16:01:44 -0700

    fix: sg for rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -15,17 +15,18 @@ resource "aws_db_instance" "main_db_instance" {
   publicly_accessible     = false
   backup_retention_period = 7
 
-  identifier            = "${var.prefix}-rds-postgres-${var.environment}"
-  allocated_storage     = 20
-  max_allocated_storage = 100
-  storage_type          = "gp3"
-  engine                = "postgres"
-  engine_version        = "16.3"
-  instance_class        = "db.t4g.micro"
-  username              = var.rds_username
-  password              = var.rds_password
-  db_name               = var.rds_db_name
-  db_subnet_group_name  = aws_db_subnet_group.main_db_subnet_group.name
+  identifier             = "${var.prefix}-rds-postgres-${var.environment}"
+  allocated_storage      = 20
+  max_allocated_storage  = 100
+  storage_type           = "gp3"
+  engine                 = "postgres"
+  engine_version         = "16.3"
+  instance_class         = "db.t4g.micro"
+  username               = var.rds_username
+  password               = var.rds_password
+  db_name                = var.rds_db_name
+  db_subnet_group_name   = aws_db_subnet_group.main_db_subnet_group.name
+  vpc_security_group_ids = [var.rds_sg_id]
 
   tags = {
     Name        = "${var.prefix}-rds-postgres-${var.environment}"
